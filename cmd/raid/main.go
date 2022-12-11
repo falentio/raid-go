b9768c11ca5c58db600edf750b5d6ef2a093cbf8
falentio <riv.kevinfalent.io@gmail.com>
2022-12-11 15:42:32 +0700

    chore: renovating

--- cmd/raid/main.go
+++ cmd/raid/main.go
@@ -11,15 +11,15 @@ import (
 
 func main() {
 	start := time.Now()
-	raids := make([]raid.Raid, 1000)
-	for i := 0; i < 1000; i++ {
-		raids[i] = raid.NewRaid().WithMessage(2131)
+	raids := make([]raid.Raid, 100000)
+	for i := 0; i < 100000; i++ {
+		raids[i] = raid.NewRaid().WithMessage(2131).WithPrefix("ppp")
 	}
 	finishCreate := time.Now()
 	for i := 0; i < 1000; i++ {
 		fmt.Println(raids[i].String())
 	}
 	finishPrint := time.Now()
-	fmt.Fprintf(os.Stderr, "1000 raid generated in %d ms\n", finishCreate.Sub(start).Milliseconds())
+	fmt.Fprintf(os.Stderr, "100,000 raid generated in %d ms\n", finishCreate.Sub(start).Milliseconds())
 	fmt.Fprintf(os.Stderr, "1000 raid printed in %d ms\n", finishPrint.Sub(finishCreate).Milliseconds())
 }
